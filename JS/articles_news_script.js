05b1bdc2e50ce5c1508ae15584230d1559dfe47d
Filipe Aguilar Apolinário Correia <120686837+Fiagapcoo@users.noreply.github.com>
2023-12-08 15:34:45 +0000

    add a map instead a for loop for the handleArticles

--- JS/articles_news_script.js
+++ JS/articles_news_script.js
@@ -26,46 +26,40 @@ fetch('../assets/JSON/articles.json')
  *          the 'card-container' element on the web page.                          *
  **********************************************************************************/
 function handleArticles() {
-    // Get the number of articles in the Articles array
-    let size = Articles.length;
-    console.log(size);
 
-    // Loop through each article and generate HTML content
-    for (let i = 0; i < size; i++) {
-        let article = Articles[i];
-        console.log(article);
-        console.log(article.IMG);
+  // Map over each article and generate HTML content
+  const articlesHTML = Articles.map((article, index) => `
+      <article class="card">
+          <div class='card-background'>
+              <img src="../assets/imgs/${article.IMG}" alt="Card_background" />
+          </div>
+          <div class='content'>
+              <h1>${article.Titulo}</h1>
+              <p>${article.Conteudo}</p>
+          </div>
+          <div class="action-bottom-bar">
+              <a href="info_article_news.html?id=${index}">
+                  Saiba Mais
+                  <svg xmlns="http://www.w3.org/2000/svg" class="chevron" width="24" height="24"
+                      viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                      stroke-linecap="round" stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                      <path d="M9 6l6 6l-6 6"></path>
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="arrow" width="24" height="24" viewBox="0 0 24 24"
+                      stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                      stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                      <path d="M5 12l14 0"></path>
+                      <path d="M15 16l4 -4"></path>
+                      <path d="M15 8l4 4"></path>
+                  </svg>
+              </a>
+          </div>
+      </article>
+  `);
 
-        // Append the generated HTML content to the 'card-container' element
-        document.getElementById('card-container').innerHTML += `
-            <article class="card">
-                <div class='card-background'>
-                    <img src="../assets/imgs/${article.IMG}" alt="Card_background" />
-                </div>
-                <div class='content'>
-                    <h1>${article.Titulo}</h1>
-                    <p>${article.Conteudo}</p>
-                </div>
-                <div class="action-bottom-bar">
-                    <a href="info_article_news.html?id=${i}">
-                        Saiba Mais
-                        <svg xmlns="http://www.w3.org/2000/svg" class="chevron" width="24" height="24"
-                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                            stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M9 6l6 6l-6 6"></path>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="arrow" width="24" height="24" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M5 12l14 0"></path>
-                            <path d="M15 16l4 -4"></path>
-                            <path d="M15 8l4 4"></path>
-                        </svg>
-                    </a>
-                </div>
-            </article>
-        `;
-    }
+  // Join the generated HTML content and append it to the 'card-container' element
+  document.getElementById('card-container').innerHTML = articlesHTML.join('');
 }
+
